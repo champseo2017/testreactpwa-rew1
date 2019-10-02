ffc5d44f96da517c43a84ae70fd1c0c4719c5fe7
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-10-02 11:05:38 +0700

    update

---
+++ custom-sw.js
@@ -0,0 +1,78 @@
+importScripts("/precache-manifest.30cd3c85d2a8303a4ce56785e46030de.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/* eslint-disable no-undef */
+// See https://developers.google.com/web/tools/workbox/guides/configure-workbox
+workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
+
+self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+
+workbox.routing.registerRoute(
+  new RegExp('.css$'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'poc-cache-Stylesheets',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 7, // cache for one week
+        maxEntries: 20, // only cache 20 request
+        purgeOnQuotaError: true,
+      }),
+    ],
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('https://jsonplaceholder.typicode.com/users'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'poc-cache-Stylesheetsusers',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 7, // cache for one week
+        maxEntries: 20, // only cache 20 request
+        purgeOnQuotaError: true,
+      }),
+    ],
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('.(png|svg|jpg|jpeg)$'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'poc-cache-Images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 7,
+        maxEntries: 50,
+        purgeOnQuotaError: true,
+      }),
+    ],
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('^https://jsonplaceholder\\.typicode\\.com/users/'),
+  workbox.strategies.CacheFirst({
+    cacheName: 'user-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      })
+    ]
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp('/user'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'poc-cache-user',
+    cacheExpiration: {
+      maxAgeSeconds: 60 * 30, //cache the news content for 30mn
+    },
+  }),
+);
+
+
+workbox.routing.registerRoute('/', workbox.strategies.networkFirst());
+// We need this in Webpack plugin (refer to swSrc option): https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin#full_injectmanifest_config
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
